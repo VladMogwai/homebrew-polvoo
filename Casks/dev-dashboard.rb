610851c7a6df180a21a50f83f6123c066e8fb648
VladMogwai <vlad.arefiev@gmail.com>
2026-03-16 23:14:40 +0000

    Release v1.0.4

--- Casks/dev-dashboard.rb
+++ Casks/dev-dashboard.rb
@@ -1,12 +1,12 @@
 cask "dev-dashboard" do
-  version "1.0.3"
+  version "1.0.4"
 
   if Hardware::CPU.arm?
-    sha256 "3dc57f1c440e0689fa7140e41528b4ace1a9a86980df18409bc5e4b73024175d"
-    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.3/Dev.Dashboard-1.0.3-arm64-mac.zip"
+    sha256 "d7eb86b7f95e13a3ae949e63819623190c20389b503e2c656950575fd8f90d50"
+    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.4/Dev.Dashboard-1.0.4-arm64-mac.zip"
   else
-    sha256 "a11d510196ccdabf3f73b162d408cd7283c8e0f06d13ab26c5ed263c9a2f358f"
-    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.3/Dev.Dashboard-1.0.3-mac.zip"
+    sha256 "ffea529f9a284555f0921e8dc66773d612bdbb172275c752c7f6817059c98d88"
+    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.4/Dev.Dashboard-1.0.4-mac.zip"
   end
 
   name "Dev Dashboard"
