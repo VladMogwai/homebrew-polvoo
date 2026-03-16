665aae6f0b8c74aa81420ecce3862c414a5ebeea
VladMogwai <vlad.arefiev@gmail.com>
2026-03-16 19:47:07 +0000

    Release v1.0.2

--- Casks/dev-dashboard.rb
+++ Casks/dev-dashboard.rb
@@ -1,13 +1,8 @@
 cask "dev-dashboard" do
-  version "1.0.1"
+  version "1.0.2"
 
-  if Hardware::CPU.arm?
-    sha256 "9438ecf1439e4d6310e8803261efabdabf7a148951b73a22f07da835a8f3d1b6"
-    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.1/Dev.Dashboard-1.0.1-arm64-mac.zip"
-  else
-    sha256 "0a087423bbcd26cd670cadf0899b4717bcf45dd30e36bc1ee13f2293042d6203"
-    url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.1/Dev.Dashboard-1.0.1-mac.zip"
-  end
+  sha256 "df734c1733af195996527de7b897b431db2f31fa92a1432e4a5a53535c92a31f"
+  url "https://github.com/VladMogwai/dev-dashboard/releases/download/v1.0.2/Dev.Dashboard-1.0.2-arm64-mac.zip"
 
   name "Dev Dashboard"
   desc "Developer Project Dashboard — like Docker Desktop for local dev projects"
